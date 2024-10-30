0bfcd78d84dc0ad97384784bb545a73dddf9ac9d
T-bit <tibit.michelangelo@gmail.com>
2024-10-30 23:03:57 +0300

    Added AssetService

---
+++ Assets/TestOfferPopup/Scripts/Runtime/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TestOfferPopup.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static bool TryRemoveValue<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, out TValue value)
+        {
+            if (!self.TryGetValue(key, out value))
+            {
+                return false;
+            }
+
+            self.Remove(key);
+
+            return true;
+        }
+    }
+}
---
+++ Assets/TestOfferPopup/Scripts/Runtime/Services/AssetService/IAssetService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace TestOfferPopup.Services
+{
+    /// <summary>
+    /// Service responsible for operations with assets, e.g. loading, unloading, etc.
+    /// </summary>
+    public interface IAssetService : IService
+    {
+        UniTask<Object> LoadAsync(Reference reference, CancellationToken cancellationToken);
+
+        void Unload(Reference reference);
+    }
+}
