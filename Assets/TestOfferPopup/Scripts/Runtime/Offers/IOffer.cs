2a6414cf73bad8b5f46e3dbd8c12a4161c706958
T-bit <tibit.michelangelo@gmail.com>
2024-10-31 08:37:28 +0300

    Added Offers

---
+++ Assets/TestOfferPopup/Scripts/Runtime/Offers/IOffer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestOfferPopup.Offers
+{
+    public interface IOffer : IAddressable<IOffer>
+    {
+        string Title { get; }
+
+        string Description { get; }
+
+        Reference<Sprite> IconReference { get; }
+
+        float Price { get; }
+
+        float Discount { get; }
+
+        IEnumerable<OfferConsumable> Consumables { get; }
+    }
+}
